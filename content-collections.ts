773a8e3ce31d620d907dc9c6f1297d3dbd16bcac
MOITY <ouxiangming_moi@foxmail.com>
2025-04-13 10:50:24 +0800

    feat: add annotate

--- content-collections.ts
+++ content-collections.ts
@@ -1,9 +1,11 @@
 import { defineCollection, defineConfig } from "@content-collections/core";
 
 const blogs = defineCollection({
+  // 生成的文件名规则是 all + 集合名称（首字母大写）
   name: "blogs",
   directory: "src/content/blog",
   include: "**/*.md",
+  // 根据配置的 schema 验证每个文件的 frontmatter（文件头部的元数据）
   schema: (z) => ({
     title: z.string(),
     date: z.string(),
