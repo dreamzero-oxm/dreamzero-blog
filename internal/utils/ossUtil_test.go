d51a6ec3e4ff2fbf36fcb03cda97b7dce770e66c
MOITY <ouxiangming_moi@foxmail.com>
2025-04-18 14:58:58 +0800

    feat: update the test file

--- internal/utils/ossUtil_test.go
+++ internal/utils/ossUtil_test.go
@@ -2,27 +2,22 @@ package utils
 
 import (
 	"blog-server/internal/config"
-	"blog-server/internal/oss"
 	"blog-server/internal/logger"
-	"testing"
+	"blog-server/internal/oss"
 	"context"
 	"fmt"
+	"os"
+	"testing"
 )
 
 func TestMakeBucket(t *testing.T) {
 	// init logger
 	logger.InitLogger()
 	// 测试前准备：初始化配置和 minio 客户端
-	conf := config.MinioConfig{
-		Endpoint: "10.21.23.14:10004",
-		AccessKeyID: "***REMOVED***",
-		SecretAccessKey: "***REMOVED***",
-		UseSSL: false,
-		BucketName: "moity-blog",
-		Location: "cn-north-1",
+	if err := config.Init("../../config/config_original.yaml"); err!= nil {
+		t.Fatalf("初始化配置文件失败: %v", err)
 	}
-	fmt.Println(conf)
-	if err := oss.InitMinIO(conf); err != nil {
+	if err := oss.InitMinIO(config.Conf.Minio); err != nil {
 		t.Fatalf("初始化 Minio 客户端失败: %v", err)
 	}
 	tests := []struct {
@@ -61,6 +56,58 @@ func TestMakeBucket(t *testing.T) {
 	// 测试后清理：删除测试用的桶
 	ctx := context.Background()
 	if err := oss.MINIMO_CLIENT.RemoveBucket(ctx, "test-bucket"); err != nil {
+		t.Logf("清理测试桶失败: %v", err)
+	}
+}
+
+func TestUploadFile(t *testing.T) {
+	// init logger
+	logger.InitLogger()
+	// 测试前准备：初始化配置和 minio 客户端
+	if err := config.Init("../../config/config_original.yaml"); err!= nil {
+		t.Fatalf("初始化配置文件失败: %v", err)
+	}
+	if err := oss.InitMinIO(config.Conf.Minio); err!= nil {
+		t.Fatalf("初始化 Minio 客户端失败: %v", err)
+	}
+
+	tests := []struct {
+		name       string
+		filePath string
+		wantErr    bool
+	}{
+		{
+			name:       "上传文件1",
+			filePath: 	"../../test_files/test.png",
+			wantErr:    false,
+		},
+	}
+
+	// 测试前准备：创建测试用的桶
+	if err := MakeBucket("test-bucket"); err!= nil {
+		t.Fatalf("创建测试桶失败: %v", err)
+	}
+
+	contentType := "application/octet-stream"
+	for i, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file, err := os.Open(tt.filePath)
+			if err!= nil {
+				t.Fatalf("打开文件失败: %v", err)
+			}
+			defer file.Close()
+			err = UploadFile("test-bucket", fmt.Sprintf("testfile-%v", i), file, contentType)
+			if (err!= nil)!= tt.wantErr {
+				t.Errorf("UploadFile() error = %v, wantErr %v", err, tt.wantErr)
+			}else{
+				t.Logf("UploadFile() success")
+			}
+		})
+	}
+
+	// 测试后清理：删除测试用的桶
+	ctx := context.Background()
+	if err := oss.MINIMO_CLIENT.RemoveBucket(ctx, "test-bucket"); err!= nil {
 		t.Logf("清理测试桶失败: %v", err)
 	}
 }
