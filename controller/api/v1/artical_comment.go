3aad0f55b66931c20aa349214f4ccf1e60384398
This-MOI(OXM) <ouxiangming_moi@foxmail.com>
2025-04-22 17:49:52 +0800

    feat: add list comment

--- controller/api/v1/artical_comment.go
+++ controller/api/v1/artical_comment.go
@@ -11,11 +11,21 @@ import (
 type ArticalCommentController struct {
 }
 
+// @Summary 添加评论
+// @Description 添加评论
+// @Tags artical_comment
+// @Accept json
+// @Produce json
+// @Param comment body service.AddCommentService true "评论内容"
+// @Param artical_title body service.AddCommentService true "文章标题列表"
+// @Success 200 {object} internal.Response{data=string}
+// @Failure 20301 {object} internal.Response{data=string}
+// @Router /artical_comment/add [post]
 func AddComment(c *gin.Context) {
 	var service service.AddCommentService
 	if err := c.ShouldBind(&service); err == nil {
 		if res := service.AddComment(); res != nil {
-			internal.APIResponse(c, IError.ErrArticalCommentCreateFailed, nil)
+			internal.APIResponse(c, IError.ErrArticalCommentCreateFailed, err.Error())
 		}
 		internal.APIResponse(c, IError.OK, nil)
 	} else {
@@ -23,8 +33,34 @@ func AddComment(c *gin.Context) {
 	}
 }
 
+// @Summary 获取评论列表
+// @Description 获取评论列表
+// @Tags artical_comment
+// @Accept json
+// @Produce json
+// @Param artical_title query string false "文章标题列表"
+// @Param is_notify query bool false "是否通知"
+// @Param is_read query bool false "是否已读"
+// @Param is_pass query bool false "是否通过审核"
+// @Param notify_desc query bool false "通知排序"
+// @Param read_desc query bool false "已读排序"
+// @Param pass_desc query bool false "通过审核排序"
+// @Success 200 {object} internal.Response{data=[]models.ArticleComment}
+// @Failure 20302 {object} internal.Response{data=string}
+// @Router /artical_comment/list [get]
+func ListComment(c *gin.Context) {
+	var service service.ListCommentService
+	if err := c.ShouldBind(&service); err == nil {
+		if res, err := service.ListComment(); err!= nil {
+			internal.APIResponse(c, IError.ErrArticalCommentListFailed, err.Error())
+		} else {
+			internal.APIResponse(c, IError.OK, res)}
+	}
+}
+
 func (controller *ArticalCommentController) InitRouter(router *gin.RouterGroup) error {
 	router = router.Group("/artical_comment")
 	router.POST("/add", AddComment)
+	router.GET("/list", ListComment)
 	return nil
 }
--- service/artical_comment.go
+++ service/artical_comment.go
@@ -3,6 +3,7 @@ package service
 import (
 	"blog-server/internal/models"
 	"encoding/json"
+	"fmt"
 	// "blog-server/internal/logger"
 )
 
@@ -26,4 +27,79 @@ func (ac *AddCommentService) AddComment() error {
 		return err
 	}
 	return nil
+}
+
+type ListCommentService struct {
+	// 评论ID
+	CommentID    int      `json:"comment_id" form:"comment_id"`
+	// 文章标题数组
+	ArticalTitle []string `json:"artical_title" form:"artical_title"`
+	// 相关内容
+	Content      string   `json:"content" form:"content"`
+	// 是否通知
+	IsNotify     bool     `json:"is_notify" form:"is_notify"`
+	// 是否已读
+	IsRead       bool     `json:"is_read" form:"is_read"`
+	// 是否通过审核
+	IsPass       bool     `json:"is_pass" form:"is_pass"`
+	// 排序
+	NotifyDesc   bool     `json:"notify_desc" form:"notify_desc"`
+	ReadDesc     bool     `json:"read_desc" form:"read_desc"`
+	PassDesc     bool     `json:"pass_desc" form:"pass_desc"`
+}
+
+func (lc *ListCommentService) ListComment() ([]models.ArticleComment, error) {
+	var comments []models.ArticleComment
+	db := models.DB
+
+	fmt.Println(lc)
+	// 如果提供了文章标题，添加标题过滤条件
+	if len(lc.ArticalTitle) > 0 {
+		jsonTitle, err := json.Marshal(lc.ArticalTitle)
+		if err != nil {
+			return nil, err
+		}
+		db = db.Where("article_title = ?", string(jsonTitle))
+	}
+
+	if lc.CommentID > 0 {
+		db = db.Where("id =?", lc.CommentID)
+	}
+
+	if lc.Content != "" {
+		db = db.Where("content LIKE ?", "%"+lc.Content+"%")
+	}
+
+	// 添加通知状态过滤
+	if lc.IsNotify {
+		db = db.Where("is_notify = ?", true)
+	}
+
+	// 添加已读状态过滤
+	if lc.IsRead {
+		db = db.Where("is_read = ?", true)
+	}
+
+	// 添加审核状态过滤
+	if lc.IsPass {
+		db = db.Where("is_pass = ?", true)
+	}
+
+	// 添加排序条件
+	if lc.NotifyDesc {
+		db = db.Order("is_notify DESC")
+	}
+	if lc.ReadDesc {
+		db = db.Order("is_read DESC")
+	}
+	if lc.PassDesc {
+		db = db.Order("is_pass DESC")
+	}
+
+	// 执行查询
+	if err := db.Find(&comments).Error; err != nil {
+		return nil, err
+	}
+
+	return comments, nil
 }
