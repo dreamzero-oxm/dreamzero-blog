8d9b983a1b5d0b4f093b9199a97d9fb072e92105
MOITY <ouxiangming_moi@foxmail.com>
2025-04-24 20:27:33 +0800

    feat: update the release file

--- release.sh
+++ release.sh
@@ -5,5 +5,6 @@ swag init
 make
 systemctl stop blog-server
 cp ./build/blog-server /etc/moity-blog/blog-server
+cp ./config/config_dev.yaml /etc/moity-blog/config.yaml
 systemctl start blog-server
 systemctl status blog-server
