41a429968a312840f9a987fbfde82c5b30e947cd
This.MOI <ouxiangming_moi@foxmail.com>
2025-05-13 19:37:19 +0800

    feat: update

--- release.sh
+++ release.sh
@@ -3,8 +3,3 @@ git pull
 go mod tidy
 swag init
 make
-systemctl stop blog-server
-cp ./build/blog-server /etc/moity-blog/blog-server
-cp ./config/config_dev.yaml /etc/moity-blog/config.yaml
-systemctl start blog-server
-systemctl status blog-server
