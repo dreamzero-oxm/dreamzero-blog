59add5cf2c5d298d89cc2bd2bcc54e881e367845
DreamZero <ouxiangming@dreamzero.cn>
2025-10-14 01:17:53 +0800

    [feat] update the frontend config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,6 +8,13 @@ const nextConfig: NextConfig = {
     // 在开发环境进行 ESLint 检查，在生产环境忽略 ESLint 错误
     ignoreDuringBuilds: process.env.NODE_ENV === 'production',
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: process.env.NODE_ENV === 'production',
+  },
 };
 
 const withMDX = createMDX({
