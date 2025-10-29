00e3c0f5423a69a0e89a6e3c4ba72fbc72807b2e
MOITY <ouxiangming_moi@foxmail.com>
2025-10-29 15:58:44 +0800

    [fix] fix some error

--- frontend/src/app/(manage)/manage/layout.tsx
+++ frontend/src/app/(manage)/manage/layout.tsx
@@ -37,10 +37,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <ManageSidebar />
-      <main className="flex-1">
-        <SidebarTrigger />
-        {children}
-      </main>
+      <div className="flex-1 flex flex-col">
+        <div className="h-14 border-b flex items-center px-4 bg-background">
+          <SidebarTrigger className="mr-2" />
+          <h1 className="text-lg font-semibold">管理后台</h1>
+        </div>
+        <div className="flex-1 p-4">
+          {children}
+        </div>
+      </div>
     </SidebarProvider>
+    
   )
 }
